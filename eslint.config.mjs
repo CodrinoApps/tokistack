a8115af0c5934f161d9069ea593c08a6d10ff89c
Florian Soller <flsoller@gmail.com>
2026-02-08 14:45:49 +0100

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import eslintPluginJest from "eslint-plugin-jest";
+import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    ignores: [
+      "**/node_modules/*",
+      "**/dist/*",
+      "**/build/*",
+      "**/coverage/*",
+      "**/jest.config.js",
+      "**/.gitignore",
+      ".github/workflows/*",
+      "apps/e2e/playwright-report/*",
+      "apps/e2e/test-results/*",
+      ".changelog.config.cjs",
+    ],
+  },
+  {
+    rules: {
+      "no-console": "error",
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+    },
+  },
+  {
+    plugins: {
+      jest: eslintPluginJest,
+    },
+  },
+);
